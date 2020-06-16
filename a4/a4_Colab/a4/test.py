26a9197a2f79fc937a19834e9c2e938b527d72be
Skypower <skypowerr3@gmail.com>
2020-06-16 10:26:40 +0800

    Assignment 4 (trained)

---
+++ a4/a4_Colab/a4/test.py
@@ -0,0 +1,11 @@
+import torch
+ 
+x = torch.tensor([[1, 2, 3, 4],[5,6,7,8]])
+print(x.size())
+print(x)
+print(torch.unsqueeze(x, dim=0).size())
+print(torch.unsqueeze(x, 0))
+print(torch.unsqueeze(x, 1).size())
+print(torch.unsqueeze(x, 1))
+print(torch.unsqueeze(x, 2).size())
+print(torch.squeeze(torch.unsqueeze(x, 2),dim=2))
